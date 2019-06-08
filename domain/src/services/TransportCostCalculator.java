304a67fa19e62640900429bf9a9c7d674820a3d6
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2019-06-08 13:19:56 -0300

    Implementing the calculation considering the burden addicional cost when the weight ton is greater than 5

--- domain/src/services/TransportCostCalculator.java
+++ domain/src/services/TransportCostCalculator.java
@@ -5,13 +5,22 @@ import valueObjects.TransportData;
 public class TransportCostCalculator {
 
     public double calculate(TransportData transportData) {
+        double burdenAdditionalCost = 0;
+
         final double pavedRoadCostByKilometer = 0.54;
         final double pavedRoadCost = transportData.getDistanceInPavementRoad() * pavedRoadCostByKilometer;
 
         final double unpavedRoadCostByKilometer = 0.62;
         final double unpavedRoadCost = transportData.getDistanceInUnpavementRoad() * unpavedRoadCostByKilometer;
 
+        if (transportData.getWeightTon() > 5) {
+            final int totalDistance = transportData.getDistanceInPavementRoad() + transportData.getDistanceInUnpavementRoad();
+            final double burdenCost = 0.02d;
+            final int excessWeight = transportData.getWeightTon() - 5;
 
-        return pavedRoadCost + unpavedRoadCost;
+            burdenAdditionalCost = burdenCost * excessWeight * totalDistance;
+        }
+
+        return pavedRoadCost + unpavedRoadCost + burdenAdditionalCost;
     }
 }
--- domain/test/services/TransportCostCalculatorTest.java
+++ domain/test/services/TransportCostCalculatorTest.java
@@ -42,4 +42,29 @@ public class TransportCostCalculatorTest {
         assertThat(transportCost, is(62d));
     }
 
+    @Test
+    public void should_calculate_the_cost_without_additional_cost_when_the_weight_ton_is_less_than_or_equal_to_five()
+    {
+        TransportData transportData = new TransportData();
+        transportData.setVehicleId(1);
+        transportData.setDistanceInUnpavementRoad(100);
+        transportData.setWeightTon(5);
+
+        double transportCost = new TransportCostCalculator().calculate(transportData);
+
+        assertThat(transportCost, is(62d));
+    }
+
+    @Test
+    public void should_calculate_the_cost_with_additional_cost_when_the_weight_ton_is_greater_than_five()
+    {
+        TransportData transportData = new TransportData();
+        transportData.setVehicleId(1);
+        transportData.setDistanceInUnpavementRoad(100);
+        transportData.setWeightTon(8);
+
+        double transportCost = new TransportCostCalculator().calculate(transportData);
+
+        assertThat(transportCost, is(68d));
+    }
 }
