5874ebf0a9c6edd3bdb81a60315a263d7bf931f5
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2019-06-09 13:25:14 -0300

    Creates tests to ensure that observation will be empty for no invoice number provided; Creates test to ensure that is possible do generate observation for more than one invoice number provided

--- tests/src/observation/generators/ObservationGeneratorTest.java
+++ tests/src/observation/generators/ObservationGeneratorTest.java
@@ -10,10 +10,12 @@ import static org.junit.Assert.assertThat;
 public class ObservationGeneratorTest {
 
     @Test
-    public void should_be_possible_to_instantiate_the_generator() {
+    public void should_return_empty_text_for_no_invoice_number_provided() {
         ObservationGenerator generator = new ObservationGenerator();
 
-        Assert.assertNotNull(generator);
+        String observation = generator.geraObservacao(Arrays.asList());
+
+        assertThat(observation, is(""));
     }
 
     @Test
@@ -23,5 +25,14 @@ public class ObservationGeneratorTest {
         String observation = generator.geraObservacao(Arrays.asList(1));
 
         assertThat(observation, is("Fatura da nota fiscal de simples remessa: 1."));
+    }
+
+    @Test
+    public void should_be_possible_to_generate_observation_to_more_than_one_invoice() {
+        ObservationGenerator generator = new ObservationGenerator();
+
+        String observation = generator.geraObservacao(Arrays.asList(1,2,3,4,5));
+
+        assertThat(observation, is("Fatura das notas fiscais de simples remessa: 1, 2, 3, 4 e 5."));
     }
 }
