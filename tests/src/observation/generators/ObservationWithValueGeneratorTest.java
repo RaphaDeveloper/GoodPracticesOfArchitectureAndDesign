c7013ebf8cabd7138095e940a9e88ec659b26372
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2019-06-09 21:33:35 -0300

    Refactoring to improve the tests

--- tests/src/observation/generators/ObservationWithValueGeneratorTest.java
+++ tests/src/observation/generators/ObservationWithValueGeneratorTest.java
@@ -21,6 +21,20 @@ public class ObservationWithValueGeneratorTest {
     }
 
     @Test
+    public void should_return_empty_text_for_no_invoice_number_provided() {
+        String observation = generator.generateFromInvoiceNumbers(Arrays.asList());
+
+        assertThat(observation, is(""));
+    }
+
+    @Test
+    public void should_return_empty_text_for_invoice_numbers_provided_equals_to_null() {
+        String observation = generator.generateFromInvoiceNumbers(null);
+
+        assertThat(observation, is(""));
+    }
+
+    @Test
     public void should_be_possible_to_generate_observation_with_the_invoice_value_for_one_invoice_number_provided() {
         String observation = generator.generateFromInvoiceNumbers(Arrays.asList(1));
 
@@ -29,8 +43,8 @@ public class ObservationWithValueGeneratorTest {
 
     @Test
     public void should_be_possible_to_generate_observation_with_the_invoice_value_for_more_than_one_invoice_number_provided() {
-        String observation = generator.generateFromInvoiceNumbers(Arrays.asList(1, 2));
+        String observation = generator.generateFromInvoiceNumbers(Arrays.asList(1, 2, 3));
 
-        assertThat(observation, is("Fatura das notas fiscais de simples remessa: 1 cujo valor é R$ 10,00 e 2 cujo valor é R$ 20,00. Total = R$ 30,00."));
+        assertThat(observation, is("Fatura das notas fiscais de simples remessa: 1 cujo valor é R$ 10,00, 2 cujo valor é R$ 20,00 e 3 cujo valor é R$ 30,00. Total = R$ 60,00."));
     }
 }
